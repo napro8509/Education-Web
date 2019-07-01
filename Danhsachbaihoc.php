ee78db207275b91bdb49a5ed39933e1d601d7138
Jonah Le <legiona14@gmail.com>
2019-07-01 23:58:39 +0700

    Update

--- Danhsachbaihoc.php
+++ Danhsachbaihoc.php
@@ -1,198 +1,194 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Course - Elements</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Course Project">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/elements_styles.css">
-<link rel="stylesheet" type="text/css" href="styles/elements_responsive.css">
+    <title>Course - Elements</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Course Project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+    <link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="styles/elements_styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/elements_responsive.css">
 </head>
 <body>
-<?php 
+<?php
 include('inc/myconnect.php');
 include('inc/function.php');
 ?>
 
 <div class="super_container">
 
-	<?php include("teacher_header.php"); ?>
-	
-	<!-- Menu -->
-	<?php include("menu.php"); ?>
+    <?php include("teacher_header.php"); ?>
 
-			</div>
+    <!-- Menu -->
+    <?php include("menu.php"); ?>
 
-		</div>
+</div>
 
-	</div>
-	
-	<!-- Home -->
+</div>
 
-	<div class="home">
-		<div class="home_background_container prlx_parent">
-			<div class="home_background prlx" style="background-image:url(images/contact_background.jpg)"></div>
-		</div>
-		<div class="home_content">
-			<h1>Danh sách bài học</h1>
-		</div>
-	</div>
+</div>
 
-	<!-- Elements -->
+<!-- Home -->
 
-	<div class="elements">
+<div class="home">
+    <div class="home_background_container prlx_parent">
+        <div class="home_background prlx" style="background-image:url(images/contact_background.jpg)"></div>
+    </div>
+    <div class="home_content">
+        <h1>Danh sách bài học</h1>
+    </div>
+</div>
 
-		<!-- Progress Bars and Accordions -->
+<!-- Elements -->
 
-		<div class="pbars_accordions">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<?php
-						$query_2="SELECT * FROM monhoc";
-						$results_2=mysqli_query($dbc,$query_2);
-						kt_query($results_2,$query_2);
-						while($mh=mysqli_fetch_array($results_2,MYSQLI_ASSOC))
-						{
-						?>
-						<div class="elements_title"><?php echo $mh['TenMonHoc']; ?></div>
+<div class="elements">
 
-				<div class="row pbars_accordions_container">
-					<!-- Progress Bars & Accordions -->	
+    <!-- Progress Bars and Accordions -->
 
-					<div class="col-lg-9">
+    <div class="pbars_accordions">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <?php
+                    $query_2 = "SELECT * FROM monhoc";
+                    $results_2 = mysqli_query($dbc, $query_2);
+                    kt_query($results_2, $query_2);
+                    while ($mh = mysqli_fetch_array($results_2, MYSQLI_ASSOC)) {
+                        ?>
+                        <div class="elements_title"><?php echo $mh['TenMonHoc']; ?></div>
 
-						<!-- Accordions -->
-						<div class="elements_accordions">
+                        <div class="row pbars_accordions_container">
+                            <!-- Progress Bars & Accordions -->
 
-							<?php
-						$query_1="SELECT * FROM tblchuong WHERE MaMonHoc=".$mh['MaMonHoc'];
-						$results_1=mysqli_query($dbc,$query_1);
-						kt_query($results_1,$query_1);
-						while($ch=mysqli_fetch_array($results_1,MYSQLI_ASSOC))
-						{
-							$count="SELECT count(MaChiTiet) FROM chitietcongthuc WHERE MaChuong=".$ch['MaChuong'];
-									$results_count=mysqli_query($dbc,$count);
-									kt_query($results_count,$count);
-									$count_n=mysqli_fetch_array($results_count,MYSQLI_ASSOC)
-						?>
-                            <div class="accordion_container">
+                            <div class="col-lg-9">
 
-	                            <div class="accordion d-flex flex-row align-items-center">
-										<?php echo $ch['TenChuong']; ?>
-										<p>Số lượt học: <?php echo $count_n['count(MaChiTiet)']; ?></p>
-									</div>
-								<div class="accordion_panel">
+                                <!-- Accordions -->
+                                <div class="elements_accordions">
 
-									<?php
-									$query="SELECT * FROM chitietcongthuc WHERE MaChuong=".$ch['MaChuong'];
-									$results=mysqli_query($dbc,$query);
-									kt_query($results,$query);
+                                    <?php
+                                    $query_1 = "SELECT * FROM tblchuong WHERE MaMonHoc=" . $mh['MaMonHoc'];
+                                    $results_1 = mysqli_query($dbc, $query_1);
+                                    kt_query($results_1, $query_1);
+                                    while ($ch = mysqli_fetch_array($results_1, MYSQLI_ASSOC)) {
+                                        $count = "SELECT count(MaChiTiet) FROM chitietcongthuc WHERE MaChuong=" . $ch['MaChuong'];
+                                        $results_count = mysqli_query($dbc, $count);
+                                        kt_query($results_count, $count);
+                                        $count_n = mysqli_fetch_array($results_count, MYSQLI_ASSOC)
+                                        ?>
+                                        <div class="accordion_container">
+                                            <div class="accordion d-flex flex-row align-items-center"
+                                                 style="justify-content: space-between; padding-right: 90px;">
+                                                <p style="margin: 0px"> <?php echo $ch['TenChuong']; ?> </p>
+                                                <p style="margin: 0px">Số lượt học: <?php echo $count_n['count(MaChiTiet)']; ?></p>
+                                            </div>
+                                            <div class="accordion_panel">
+                                                <?php
+                                                $query = "SELECT * FROM chitietcongthuc WHERE MaChuong=" . $ch['MaChuong'];
+                                                $results = mysqli_query($dbc, $query);
+                                                kt_query($results, $query);
+                                                while ($ct = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
+                                                    ?>
 
-									while($ct=mysqli_fetch_array($results,MYSQLI_ASSOC))
-									{
-										
-									?>
+                                                    <a href="lesson_details.php?ct=<?php echo $ct['MaChiTiet']; ?>">
+                                                        <div class="d-flex flex-row"
+                                                             style="justify-content: space-between;">
+                                                            <p href=""><?php echo $ct['TenCongThuc']; ?></p>
 
-										<a href="lesson_details.php?ct=<?php echo $ct['MaChiTiet']; ?>">
-										<div class="d-flex flex-row" style="justify-content: space-between;">
-											<p href=""><?php echo $ct['TenCongThuc']; ?></p>
-	<!-- 										<p>Số lượt học: <?php echo $count_n['count(MaChiTiet)']; ?>; Số câu hỏi: 50</p>
-	 -->								</div></a>
+	                                                    </div>
+                                                    </a>
 
-									<?php } ?>
-								</div>
+                                                <?php } ?>
+                                            </div>
+                                        </div>
+
+                                    <?php } ?>
+
+
+                                </div>
                             </div>
 
-                        <?php } ?>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- Loaders -->
 
-						</div>
-					</div>
+    <div class="loaders">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="elements_title">Thống kê</div>
+                </div>
+            </div>
 
-				</div>
-			<?php } ?>
-			</div>
-		</div>
+            <div class="row elements_loaders_container">
+                <div class="col-lg-3 loader_col">
+                    <!-- Loader -->
+                    <div class="loader" data-perc="0.75"></div>
+                    <div class="loader_text text-center">Photos Taken</div>
+                    <div class="loader_sub text-center">Etiam nec odio vestibulum est.</div>
+                </div>
+                <div class="col-lg-3 loader_col">
+                    <!-- Loader -->
+                    <div class="loader" data-perc="0.83"></div>
+                    <div class="loader_text text-center">Km Walked</div>
+                    <div class="loader_sub text-center">Odio vestibulum est mattis.</div>
+                    <span></span>
+                </div>
+                <div class="col-lg-3 loader_col">
+                    <!-- Loader -->
+                    <div class="loader" data-perc="0.25"></div>
+                    <div class="loader_text text-center">Cities Visited</div>
+                    <div class="loader_sub text-center">Vestibulum est mattis effic.</div>
+                </div>
+                <div class="col-lg-3 loader_col">
+                    <!-- Loader -->
+                    <div class="loader" data-perc="0.95"></div>
+                    <div class="loader_text text-center">Cities Visited</div>
+                    <div class="loader_sub text-center">Vestibulum est mattis effic.</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Milestones -->
+
+    <?php include("milestone.php"); ?>
+
+    <!-- Icon Boxes -->
+
+</div>
+
+<!-- Footer -->
+
+<footer class="footer">
+    <div class="container">
+
+        <!-- Footer Copyright -->
+
+        <div class="footer_bar d-flex flex-column flex-sm-row align-items-center">
+            <div class="footer_copyright">
+                <span>Colorlib All rights reserved</span>
+            </div>
+            <div class="footer_social ml-sm-auto">
+                <ul class="menu_social">
+                    <li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                    <li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                </ul>
             </div>
         </div>
 
-        <!-- Loaders -->
-
-		<div class="loaders">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="elements_title">Thống kê</div>
-					</div>
-				</div>
-
-				<div class="row elements_loaders_container">
-					<div class="col-lg-3 loader_col">
-						<!-- Loader -->
-						<div class="loader" data-perc="0.75"></div>
-						<div class="loader_text text-center">Photos Taken</div>
-						<div class="loader_sub text-center">Etiam nec odio vestibulum est.</div>
-					</div>
-					<div class="col-lg-3 loader_col">
-						<!-- Loader -->
-						<div class="loader" data-perc="0.83"></div>
-						<div class="loader_text text-center">Km Walked</div>
-						<div class="loader_sub text-center">Odio vestibulum est mattis.</div>
-						<span></span>
-					</div>
-					<div class="col-lg-3 loader_col">
-						<!-- Loader -->
-						<div class="loader" data-perc="0.25"></div>
-						<div class="loader_text text-center">Cities Visited</div>
-						<div class="loader_sub text-center">Vestibulum est mattis effic.</div>
-					</div>
-					<div class="col-lg-3 loader_col">
-						<!-- Loader -->
-						<div class="loader" data-perc="0.95"></div>
-						<div class="loader_text text-center">Cities Visited</div>
-						<div class="loader_sub text-center">Vestibulum est mattis effic.</div>
-					</div>
-				</div>
-
-			</div>
-		</div>
-		
-		<!-- Milestones -->
-		
-		<?php include("milestone.php"); ?>
-
-		<!-- Icon Boxes -->
-
-	</div>
-
-	<!-- Footer -->
-
-	<footer class="footer">
-		<div class="container">
-			
-			<!-- Footer Copyright -->
-
-			<div class="footer_bar d-flex flex-column flex-sm-row align-items-center">
-				<div class="footer_copyright">
-					<span>Colorlib All rights reserved</span>
-				</div>
-				<div class="footer_social ml-sm-auto">
-					<ul class="menu_social">
-						<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-					</ul>
-				</div>
-			</div>
-
-		</div>
-	</footer>
+    </div>
+</footer>
 
 </div>
 
--- courselist.php
+++ courselist.php
@@ -1,99 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Course - Courses</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Course Project">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/courses_styles.css">
-<link rel="stylesheet" type="text/css" href="styles/courses_responsive.css">
+    <title>Course - Courses</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Course Project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+    <link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="styles/courses_styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/courses_responsive.css">
 </head>
 <body>
-<?php 
+<?php
 include('inc/myconnect.php');
 include('inc/function.php');
 ?>
 <div class="super_container">
 
-	<?php include("header.php"); ?>
-	
-	<!-- Menu -->
-	<?php include("menu.php"); ?>
+    <?php include("header.php"); ?>
 
-	<!-- Home -->
+    <!-- Menu -->
+    <?php include("menu.php"); ?>
 
-	<div class="home">
-		<div class="home_background_container prlx_parent">
-			<div class="home_background prlx" style="background-image:url(images/courses_background.jpg)"></div>
-		</div>
-		<div class="home_content">
-			<h1>Courses</h1>
-		</div>
-	</div>
+    <!-- Home -->
 
-	<!-- Popular -->
+    <div class="home">
+        <div class="home_background_container prlx_parent">
+            <div class="home_background prlx" style="background-image:url(images/courses_background.jpg)"></div>
+        </div>
+        <div class="home_content">
+            <h1>Courses</h1>
+        </div>
+    </div>
 
-	<div class="popular page_section">
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					<div class="section_title text-center">
-						<h1>Chi Tiết Khóa học</h1>
-					</div>
-				</div>
-			</div>
+    <!-- Popular -->
 
-			<div class="row course_boxes">
-				<?php
-					$machuong=$_GET['chuong'];
-					$query="SELECT * FROM chitietcongthuc WHERE MaChuong=".$machuong;
-					$results=mysqli_query($dbc,$query);
-					kt_query($results,$query);
-					$query_1="SELECT count(MaChiTiet) FROM chitietcongthuc WHERE MaChuong=".$machuong;
-					$results_1=mysqli_query($dbc,$query_1);
-					kt_query($results_1,$query_1);
-					$number_1=mysqli_fetch_assoc($results_1);
-					?>
-					<div class="price_box d-flex flex-row align-items-center">
-							<div class="course_author_image">
-								<img src="images/author.jpg" alt="https://unsplash.com/@mehdizadeh">
-							</div>
-							<div class="course_author_name"><?php echo "Thầy giáo : Nguyễn Công Hoan </br>"; ?><span></span></div>
-							<div class="course_author_name"><?php echo "Số lượng bài học + bài kiểm tra : ".$number_1['count(MaChiTiet)']; ?><span></span></div>
-						</div>
-					<?php
-					while($number=mysqli_fetch_array($results,MYSQLI_ASSOC))
-					{
-				?>
-				<div class="card-body text-center">
-						<div class="card-title"><a href="lesson_details.php?ct=<?php echo $number['MaChiTiet']; ?>"><?php echo $number['TenCongThuc']; ?></a></div>
-				</div>
-				<?php
-				} ?>
-			</div>
-		</div>		
-	</div>
+    <div class="popular page_section">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="section_title text-center">
+                        <h1>Chi Tiết Khóa học</h1>
+                    </div>
+                </div>
+            </div>
 
-<?php include("footer.php"); ?>
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
-				</div>
-				<div class="footer_social ml-sm-auto">
-					<ul class="menu_social">
-						<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-					</ul>
-				</div>
-			</div>
+            <div class="row course_boxes">
+                <?php
+                $machuong = $_GET['chuong'];
+                $query = "SELECT * FROM chitietcongthuc WHERE MaChuong=" . $machuong;
+                $results = mysqli_query($dbc, $query);
+                kt_query($results, $query);
+                $query_1 = "SELECT count(MaChiTiet) FROM chitietcongthuc WHERE MaChuong=" . $machuong;
+                $results_1 = mysqli_query($dbc, $query_1);
+                kt_query($results_1, $query_1);
+                $number_1 = mysqli_fetch_assoc($results_1);
+                ?>
+                <div class="price_box d-flex flex-row align-items-center">
+                    <div class="course_author_image">
+                        <img src="images/author.jpg" alt="https://unsplash.com/@mehdizadeh">
+                    </div>
+                    <div class="course_author_name"><?php echo "Thầy giáo : Nguyễn Công Hoan </br>"; ?><span></span>
+                    </div>
+                    <div class="course_author_name"><?php echo "Số lượng bài học + bài kiểm tra : " . $number_1['count(MaChiTiet)']; ?>
+                        <span></span></div>
+                </div>
+                <div class="col-12 col-md-6" style="margin-top: 20px">
+                <ul class="list-group">
 
-		</div>
-	</footer>
+                <?php
+                while ($number = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
+                    ?>
+
+                        <a class="list-group-item d-flex justify-content-between align-items-center"
+                           href="lesson_details.php?ct=<?php echo $number['MaChiTiet']; ?>">
+                            <?php echo $number['TenCongThuc']; ?>
+                            <span class="badge badge-primary badge-pill">10</span>
+                        </a>
+
+                    <?php
+                } ?>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include("footer.php"); ?>
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+    All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a
+            href="https://colorlib.com" target="_blank">Colorlib</a>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
+</div>
+<div class="footer_social ml-sm-auto">
+    <ul class="menu_social">
+        <li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+        <li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+        <li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+    </ul>
+</div>
+</div>
+
+</div>
+</footer>
 
 </div>
 
